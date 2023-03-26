6557bf88e42765ce980cea4b4c9fe0ba6b6de13c
meghanak0803 <meghanak0803@gmail.com>
2023-03-26 08:53:46 -0400

    initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from 'react-dom'
+import MarketingApp from './components/MarketingApp'
+export default () => {
+    return <>
+            <h1>Hi There</h1>
+            <hr/>
+            <MarketingApp />
+    </>
+}
